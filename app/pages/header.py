094bf463d7809ba7a41d40d252fd78fbdc2634fd
MassiZek <49779093+MassiZek@users.noreply.github.com>
2024-01-29 15:01:45 +0100

    New changes on the uplaod file page

---
+++ app/pages/header.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from loguru import logger as log
+from streamlit_option_menu import option_menu
+from pathlib import Path
+
+DEFAULT_PAGE = "Instrument Separation"
+
+def header(logo_and_title=True):
+
+
+    if logo_and_title:
+        head = st.columns([2, 1, 5, 5])
+        with head[1]:
+            st.image("images/logo.jpg", use_column_width=False, width=70)
+        with head[2]:
+            st.markdown(
+                "<h1>Music Source Separation</h1><p><b></b></p>",
+                unsafe_allow_html=True,
+            )
+
+# Usage in your main script
+if __name__ == "__main__":
+    st.set_page_config(
+        page_title="Action Learning : Music source separation",
+        page_icon="images/logo.jpg",
+        layout="wide",
+        initial_sidebar_state="collapsed",
+    )
+    header()
